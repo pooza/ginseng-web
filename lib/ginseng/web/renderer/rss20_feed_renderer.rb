d63a41f58f019630b5fdf432930b8fdef21e8d15
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-10-01 19:42:45 +0900

    #71

--- lib/ginseng/web/renderer/rss20_feed_renderer.rb
+++ lib/ginseng/web/renderer/rss20_feed_renderer.rb
@@ -20,10 +20,10 @@ module Ginseng
           entries.each do |entry|
             maker.items.new_item do |item|
               if info = fetch_image(entry.dig(:enclosure, :url))
-                info.each {|k, v| item.enclosure&.send("#{k}=", v)}
+                info.each {|k, v| item.enclosure.send("#{k}=", v)}
                 entry.delete(:enclosure)
               end
-              entry.each {|k, v| item.send("#{k}=", v)}
+              entry.except(:enclosure).each {|k, v| item.send("#{k}=", v)}
             end
           rescue => e
             @logger.error(error: e, entry: entry)
