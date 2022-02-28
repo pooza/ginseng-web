86bb30068b82c60214f61fca7e6222675cc8f862
pooza <tkoishi@b-shock.co.jp>
2022-02-28 15:48:44 +0900

    #89

---
+++ bin/test.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.unshift(File.join(File.expand_path('..', __dir__), 'lib'))
+
+require 'ginseng/web'
+module Ginseng
+  module Web
+    warn Package.full_name
+    warn File.basename(__FILE__)
+    warn ''
+    TestCase.load(ARGV.getopts('', 'cases:')['cases'] || ARGV.first)
+  rescue => e
+    warn e.message
+    exit 1
+  end
+end
