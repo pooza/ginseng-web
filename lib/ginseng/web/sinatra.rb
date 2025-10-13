70d61ba820549718b0a53afb549bde231b17dbd1
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2025-10-13 09:04:21 +0900

    update: sinatra

--- lib/ginseng/web/sinatra.rb
+++ lib/ginseng/web/sinatra.rb
@@ -16,15 +16,10 @@ module Ginseng
 
       before do
         @renderer = default_renderer_class.new
-        @body = request.body.to_s
-        @headers = request.env.select {|k, _v| k.start_with?('HTTP_')}.transform_keys do |k|
-          k.sub(/^HTTP_/, '').downcase.gsub(/(^|_)\w/, &:upcase).tr('_', '-')
-        end
-        begin
-          @params = JSON.parse(@body).with_indifferent_access
-        rescue
-          @params = params.with_indifferent_access
-        end
+        @body = request.body&.read.to_s
+        request.body&.rewind
+        @headers = build_headers
+        @params = build_params
         @logger.info(request: {
           method: request.request_method,
           path: request.path,
@@ -40,6 +35,26 @@ module Ginseng
 
       get '/about' do
         @renderer.message = package_class.full_name
+        return @renderer.to_s
+      end
+
+      get '/test/get' do
+        @renderer = JSONRenderer.new
+        if Environment.test?
+          @renderer.message = @params
+        else
+          @renderer.status = 404
+        end
+        return @renderer.to_s
+      end
+
+      post '/test/post' do
+        @renderer = JSONRenderer.new
+        if Environment.test?
+          @renderer.message = @params
+        else
+          @renderer.status = 404
+        end
         return @renderer.to_s
       end
 
@@ -67,6 +82,21 @@ module Ginseng
 
       def default_renderer_class
         return JSONRenderer
+      end
+
+      def build_headers
+        return request.env.select {|k, _v| k.start_with?('HTTP_')}.transform_keys do |k|
+          k.sub(/^HTTP_/, '').downcase.gsub(/(^|_)\w/, &:upcase).tr('_', '-')
+        end
+      end
+
+      def build_params
+        return (params || {}).to_h.symbolize_keys unless request.media_type == 'application/json'
+        begin
+          return JSON.parse(@body, symbolize_names: true)
+        rescue
+          return {}
+        end
       end
     end
   end
--- test/sinatra_test.rb
+++ test/sinatra_test.rb
@@ -28,6 +28,31 @@ module Ginseng
         assert_false(last_response.ok?)
         assert_equal(404, last_response.status)
       end
+
+      def test_get
+        header 'User-Agent', Package.user_agent
+        get '/test/get?param1=aaa'
+
+        assert_predicate(last_response, :ok?)
+        params = JSON.parse(last_response.body, symbolize_names: true)
+
+        assert_kind_of(Hash, params)
+        assert_equal('aaa', params[:param1])
+        assert_nil(params[:param2])
+      end
+
+      def test_post
+        header 'User-Agent', Package.user_agent
+        header 'Content-Type', 'application/json'
+        post '/test/post', {param1: 'aaa', param2: 'bbb'}.to_json
+
+        assert_predicate(last_response, :ok?)
+        params = JSON.parse(last_response.body, symbolize_names: true)
+
+        assert_kind_of(Hash, params)
+        assert_equal('aaa', params[:param1])
+        assert_equal('bbb', params[:param2])
+      end
     end
   end
 end
